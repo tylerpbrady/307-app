9101d4ab8b0cb79b94d3fd7046f61dd23575d8d9
Tyler Brady <tylerpbrady@gmail.com>
2024-01-18 13:10:01 -0800

    added finding user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -47,6 +47,9 @@ const findUserByName = (name) => {
     );
   };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
@@ -56,8 +59,17 @@ app.get("/users", (req, res) => {
     } else{
         res.send(users); 
     }
+});
 
-  });
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result)
+    }
+});
 
 
 
