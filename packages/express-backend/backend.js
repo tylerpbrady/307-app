7eeae8a7c935e36ca2528834f9ef8c6336887f18
Tyler Brady <tylerpbrady@gmail.com>
2024-01-18 12:58:58 -0800

    Added user query

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,9 +41,25 @@ app.get("/", (req, res) => {
 });
 
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+
 app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result};
+        res.send(result);
+    } else{
+        res.send(users); 
+    }
+
   });
+
+
 
 app.listen(port, () => {
   console.log(
