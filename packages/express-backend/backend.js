344532fde39a95f77aa5b32f6485e77f138102fd
Tyler Brady <tylerpbrady@gmail.com>
2024-01-24 09:51:38 -0800

    Added POST request implementation.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -71,6 +71,17 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
 
 
 app.listen(port, () => {
